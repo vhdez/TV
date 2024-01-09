00fb0e92d8ed2c667b43aba9de0846e765958c26
vhdez <vhernandez@scienceleadership.org>
2024-01-09 13:31:16 -0500

    Make Table cell's editable.

--- src/main/java/com/example/tv/Controller.java
+++ src/main/java/com/example/tv/Controller.java
@@ -1,34 +1,84 @@
 package com.example.tv;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.BooleanStringConverter;
+import javafx.util.converter.IntegerStringConverter;
+
+import java.io.*;
 
 public class Controller {
     public TableView<KeystoneStudentData> myTableView;
+    public TableColumn<KeystoneStudentData, String> nameColumn;
+    public TableColumn<KeystoneStudentData, String> advisoryColumn;
+    public TableColumn<KeystoneStudentData, Integer> gradeColumn;
+    public TableColumn<KeystoneStudentData, Boolean> algTakingColumn;
 
     // MEETS EXPECTATIONS: Display at least 3 rows and 3 columns of data
-    public void initialize() {
-        // Programmer creates data Objects
-        KeystoneStudentData student1 = new KeystoneStudentData("Noah Frank", "Jonas", 12, false);
-        KeystoneStudentData student2 = new KeystoneStudentData("Eli Phillips", "Pahomov", 12, false);
-        System.out.println(student2);
-        // need to tell the TableView about each data object
-        // ???????
+    public void initialize() throws Exception {
+        this.restoreData();
+        // My TableView needs to have editing features turned on first:
+        myTableView.setEditable(true);
+
+        // Each column in my Table needs to be taught how to find the Object’s field for the data it displays:
+        nameColumn.setCellValueFactory(new PropertyValueFactory<KeystoneStudentData, String>("studentName"));
+        // Make each cell editable by setting up its CellFactory
+        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        // Set up an onAction method for cell edit commit event
+        nameColumn.setOnEditCommit(event -> {
+            KeystoneStudentData cellData = event.getRowValue();
+            cellData.setStudentName(event.getNewValue());
+        });
+
+        advisoryColumn.setCellValueFactory(new PropertyValueFactory<KeystoneStudentData, String>("advisory"));
+        advisoryColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        advisoryColumn.setOnEditCommit(event -> {
+            KeystoneStudentData cellData = event.getRowValue();
+            cellData.setAdvisory(event.getNewValue());
+        });
+
+        gradeColumn.setCellValueFactory(new PropertyValueFactory<KeystoneStudentData, Integer>("grade"));
+        gradeColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        gradeColumn.setOnEditCommit(event -> {
+            KeystoneStudentData cellData = event.getRowValue();
+            cellData.setGrade(event.getNewValue());
+        });
+
+        algTakingColumn.setCellValueFactory(new PropertyValueFactory<KeystoneStudentData, Boolean>("algebraTaking"));
+        algTakingColumn.setCellFactory(TextFieldTableCell.forTableColumn(new BooleanStringConverter()));
+        algTakingColumn.setOnEditCommit(event -> {
+            KeystoneStudentData cellData = event.getRowValue();
+            cellData.setAlgebraTaking(event.getNewValue());
+        });
     }
 
     // MEETS EXPECTATIONS
     public void saveData() throws Exception {
-        // open a file
-        // construct an ObjectOutputStream
-
-        // writeObject for each object
+        File fileForData = new File("MyData");
+        FileOutputStream outputStream = new FileOutputStream(fileForData);
+        ObjectOutputStream objOutputStream = new ObjectOutputStream(outputStream);
+        objOutputStream.writeObject(myTableView.getItems().size());
+        for (KeystoneStudentData eachData : myTableView.getItems()) {
+            objOutputStream.writeObject(eachData);
+        }
+        objOutputStream.flush();
+        outputStream.flush();
     }
 
     // MEETS EXPECTATIONS
     public void restoreData() throws Exception {
-        // open a file
-        // construct an ObjectInputStream
-        // readObject for each object
-
+        File fileForData = new File("MyData");
+        FileInputStream inputStream = new FileInputStream(fileForData);
+        ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
+        Integer numOfSavedObjects = (Integer)objInputStream.readObject();
+        for (int i = 0; i < numOfSavedObjects; i = i + 1) {
+            KeystoneStudentData eachData = (KeystoneStudentData) objInputStream.readObject();
+            myTableView.getItems().add(eachData);
+        }
+        inputStream.close();
     }
 
     // EXCEEDS EXPECTATIONS
--- src/main/java/com/example/tv/GUILauncher.java
+++ src/main/java/com/example/tv/GUILauncher.java
@@ -8,13 +8,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class GUILauncher extends Application {
+    Controller myController;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUILauncher.class.getResource("View.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 623);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 571);
+        myController = (Controller) fxmlLoader.getController();
         stage.setTitle("This is all of my data.");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void stop() throws Exception {
+        myController.saveData();
     }
 
     public static void main(String[] args) {
